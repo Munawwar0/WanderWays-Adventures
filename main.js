1c3fbad2dc68548dca172e4ca2308e5dbf27cdcb
Munawwar khan <92468595+Munawwar0@users.noreply.github.com>
2024-03-14 16:35:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -61,3 +61,28 @@ var swiper = new Swiper(".review-slider", {
 
     }
 });
+
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:false,
+
+    },
+    breakpoint:{
+        450:{
+            slidesPerviews: 2,
+        },
+        768:{
+            slidesPerviews: 3,
+        },
+        991:{
+            slidesPerviews: 4,
+        },
+        1200:{
+            slidesPerviews: 5,
+        },
+
+    }
+});
